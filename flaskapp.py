1dc81482a108ea4e86fc77820b9f3c4c753ea1b5
bechimcut <andythegril@gmail.com>
2024-06-03 12:55:22 +0700

    second commit

--- flaskapp.py
+++ flaskapp.py
@@ -1,171 +1,147 @@
-from datetime import datetime
-import time
-import cv2
-import numpy as np
-import imagezmq
-import threading
 import os
+import cv2
+import boto3
 
-from flask import Flask, render_template, Response, jsonify, url_for, session
-from flask_cors import CORS
-from flask import abort
-
+from flask import Flask, render_template, request, session, redirect, url_for, Response
+from dotenv import load_dotenv
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
-from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
-from threading import Lock
-from model_processing import video_detection, process_camera_stream
+from werkzeug.utils import secure_filename
+from botocore.exceptions import NoCredentialsError
+from ultralytics import YOLO
+
+load_dotenv()
 
 app = Flask(__name__)
-
-CORS(app)
-
-app.config['SECRET_KEY'] = 'bechimcut'
+app.config['SECRET_KEY'] = 'secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
+# AWS S3 Configuration
+S3_ACCESS_KEY = os.getenv('S3_ACCESS_KEY')
+S3_SECRET_KEY = os.getenv('S3_SECRET_KEY')
+S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+S3_REGION = os.getenv('S3_REGION')
+S3_FOLDER_NAME = 'front-video/'
 
-frame_dict = {}
-frame_dict_lock = Lock()
+# Boto3 client
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=S3_ACCESS_KEY,
+    aws_secret_access_key=S3_SECRET_KEY,
+    region_name=S3_REGION
+)
+
+# YOLO Model
+model = YOLO("YOLO-Weights/best.pt")
+classNames = ["accident", "non-accident"]
 
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
-    submit = SubmitField("Run")
+    submit = SubmitField("Upload")
 
 
-def generate_frames(path_x=''):
-    yolo_output = video_detection(path_x)
-    for detection_ in yolo_output:
-        ref, buffer = cv2.imencode('.jpg', detection_)
+def process_frame(frame, yolo_model, class_names, confidence_threshold=0.7):
+    results = yolo_model(frame)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            confidence = float(box.conf[0])
+            cls = int(box.cls[0])
+            class_name = class_names[cls]
 
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-
-def generate_frames_web(path_x):
-    yolo_output = video_detection(path_x)
-    for detection_ in yolo_output:
-        ref, buffer = cv2.imencode('.jpg', detection_)
-
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            if confidence >= confidence_threshold and class_name == "accident":
+                x1, y1, x2, y2 = box.xyxy[0]
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 255), 3)
+                label = f'{class_name} {confidence:.2f}'
+                cv2.putText(frame, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    return frame
 
 
-def frame_receiver():
-    global frame_dict
-    while True:
-        try:
-            receiver = imagezmq.ImageHub(open_port='tcp://*:5555')
-            print('ImageZMQ receiver started')
-            while True:
-                print("Frame receiver thread is running...")
-                camera_name, frame = receiver.recv_image()
-                print(f'ImgZMQ receiving frames from {camera_name} at {datetime.now()}')
-                print(f"Updated frame_dict with {camera_name}. Current keys: {list(frame_dict.keys())}")
-                with frame_dict_lock:
-                    print(f"Type of frame data: {type(frame)}")
-                    frame_dict[camera_name] = frame
-                    print(f"Received and stored frame for {camera_name}. Total cameras now: {len(frame_dict)}")
-                receiver.send_reply(b'OK')
-        except Exception as e:
-            print(f"General error processing video: {e}")
-            time.sleep(5)
+def video_detection(path_x):
+    cap = None
+    try:
+        cap = cv2.VideoCapture(path_x)
+        if not cap.isOpened():
+            raise ValueError("Unable to open video file")
+
+        frame_skip = 3
+        frame_count = 0
+
+        while True:
+            ret, img = cap.read()
+            if not ret:
+                break
+
+            frame_count += 1
+            if frame_count % frame_skip == 0:
+                processed_frame = process_frame(img, model, classNames)
+                try:
+                    ret, buffer = cv2.imencode('.jpg', processed_frame)
+                    if ret:
+                        frame_bytes = buffer.tobytes()
+                        yield (b'--frame\r\n'
+                               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n\r\n')
+                    else:
+                        print(f"Failed to encode frame {frame_count}")
+                except Exception as e:
+                    print(f"Exception while encoding frame {frame_count}: {e}")
+
+    except Exception as e:
+        print(f"Error processing video: {e}")
+    finally:
+        if cap:
+            cap.release()
+        cv2.destroyAllWindows()
 
 
-threading.Thread(target=frame_receiver, daemon=True).start()
+def upload_to_s3(file_path, file_name):
+    try:
+        s3_client.upload_file(
+            file_path,
+            S3_BUCKET_NAME,
+            f"{S3_FOLDER_NAME}{file_name}",
+            ExtraArgs={"ACL": "public-read"}
+        )
+        print(f"Upload Successful: {S3_FOLDER_NAME}{file_name}")
+        return f"https://{S3_BUCKET_NAME}.s3.{S3_REGION}.amazonaws.com/{S3_FOLDER_NAME}{file_name}"
+    except FileNotFoundError:
+        print("File not found")
+        return None
+    except NoCredentialsError:
+        print("Credentials not available")
+        return None
+    except Exception as e:
+        print(f"Upload failed: {str(e)}")
+        return None
 
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/home', methods=['GET', 'POST'])
 def home():
     session.clear()
-    return render_template('indexproject.html')
+    return render_template('index.html')
 
 
-@app.route('/video_feed/<camera_name>')
-def video_feed(camera_name):
-    def generate():
-        while True:
-            frame = frame_dict.get(camera_name)
-            if frame is not None:
-                # Check if frame is a NumPy array. No need to encode or reshape here.
-                if isinstance(frame, np.ndarray):
-                    print(f"Frame dimensions: {frame.shape}")
-                    # Directly encode the NumPy array to JPEG format
-                    ret, buffer = cv2.imencode('.jpg', frame)
-                    if not ret:
-                        print("Error encoding frame")
-                        continue
-
-                    frame_bytes = buffer.tobytes()
-                    yield (b'--frame\r\n'
-                           b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
-                else:
-                    print("Frame is not a NumPy array. Check the received frame format.")
-
-    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-@app.route('/camera_urls')
-def camera_urls():
-    with frame_dict_lock:
-        # frame_dict["debug_camera"] = "debug_frame"
-        print("Complete frame_dict contents:", frame_dict)
-        camera_names = list(frame_dict.keys())
-        # print("Sending camera names:", camera_names)
-    return jsonify(camera_names)
-
-
-@app.route('/FrontPage', methods=['GET', 'POST'])
-def front():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data
         filename = secure_filename(file.filename)
-        save_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'], filename)
-        try:
-            file.save(save_path)
-            session['video_path'] = save_path
-            print(f"File saved at {save_path}")
-        except Exception as e:
-            print(f"Error saving file: {e}")
-
-    return render_template('videoprojectnew.html', form=form)
+        save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(save_path)
+        session['video_path'] = save_path
+    return render_template('upload.html', form=form)
 
 
-@app.route('/all-cameras', methods=['GET', 'POST'])
-def see_all_cams():
-    try:
-        session.clear()
-        return render_template('showallcams.html')
-    except Exception as e:
-        print(e)
-        abort(5000)
+@app.route('/video')
+def video():
+    video_path = session.get('video_path')
+    if not video_path:
+        return "No video path found in session", 404
+    return Response(video_detection(video_path), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
-# @app.route('/trigger_update')
-# def trigger_update():
-#     frame_dict['manual_test_camera'] = 'manual_test_frame'
-#     return jsonify(success=True)
-
-
-@app.route('/debug/frame_dict')
-def debug_frame_dict():
-    with frame_dict_lock:
-        # current_frame_dict = dict(frame_dict)
-        # return jsonify({camera: "Frame data" for camera in current_frame_dict.keys()})
-        return jsonify(list(frame_dict.keys()))
-
-
-# with frame_dict_lock:
-#     frame_dict['dummy_camera'] = 'dummy_data'
-
-
-if __name__ == "__main__":
-    print("Starting frame receiver thread...")
-    threading.Thread(target=frame_receiver, daemon=True).start()
-    print("Frame receiver thread started.")
-    app.run(debug=True, threaded=True)
+if __name__ == '__main__':
+    app.run(debug=True)
